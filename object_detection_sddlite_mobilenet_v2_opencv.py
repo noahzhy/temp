2e6ef03f749fadb338a9bf0082c15926dce3a3af
장호우 <2018000337@hanyang.ac.kr>
2019-03-04 12:23:08 +0900

    update

---
+++ object_detection_sddlite_mobilenet_v2_opencv.py
@@ -0,0 +1,104 @@
+# Import packages
+import os
+import cv2
+import time
+import imutils
+import numpy as np
+# Import utilites
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+from imutils.video import VideoStream
+from imutils.video import FPS
+import tensorflow as tf
+
+# Set up camera constants
+IM_WIDTH = 320
+IM_HEIGHT = 240
+
+# Number of classes the object detector can identify
+NUM_CLASSES = 1
+
+## Load the label map.
+# Label maps map indices to category names, so that when the convolution
+# network predicts `5`, we know that this corresponds to `airplane`.
+# Here we use internal utility functions, but anything that returns a
+# dictionary mapping integers to appropriate string labels would be fine
+label_map = label_map_util.load_labelmap("mscoco_label_map.pbtxt")
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+# Load the Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile("frozen_inference_graph.pb", 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    sess = tf.Session(graph=detection_graph)
+
+
+# Define input and output tensors (i.e. data) for the object detection classifier
+
+# Input tensor is the image
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+# Output tensors are the detection boxes, scores, and classes
+# Each box represents a part of the image where a particular object was detected
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+# Each score represents level of confidence for each of the objects.
+# The score is shown on the result image, together with the class label.
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+# Number of objects detected
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+# Initialize frame rate calculation
+frame_rate_calc = 1
+freq = cv2.getTickFrequency()
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# Initialize camera and perform object detection.
+# Initialize Picamera and grab reference to the raw capture
+vs = VideoStream(src=0).start()
+time.sleep(2.0)
+fps = FPS().start()
+
+while True:
+    # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
+    # i.e. a single-column array, where each item in the column has the pixel RGB value
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+    frame.setflags(write=1)
+    frame_expanded = np.expand_dims(frame, axis=0)
+
+    # Perform the actual detection by running the model with the image as input
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: frame_expanded})
+
+    # Draw the results of the detection (aka 'visulaize the results')
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        frame,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8,
+        min_score_thresh=0.40)
+
+    # All the results have been drawn on the frame, so it's time to display it.
+    cv2.imshow('Object detector', frame)
+    # Press 'q' to quit
+    if cv2.waitKey(1) == ord('q'):
+        break
+    # update the FPS counter
+    fps.update()
+
+fps.stop()
+cv2.destroyAllWindows()
+vs.stop()
